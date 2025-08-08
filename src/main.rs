8a66863ebf162b42e5ac6b604c2b019f453434b7
Kyle Harris <kaharrs2@gmail.com>
2025-08-07 17:54:11 -0700

    asdf broken

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,25 @@ use noise::{Fbm, MultiFractal, NoiseFn, Perlin};
 pub mod colormap;
 use crate::colormap::*;
 
+#[derive(Clone, Debug)]
+struct Map<T> {
+    dims: (usize, usize),
+    data: Vec<T>,
+}
+
+impl<T: Clone + Default> Map<T> {
+    fn zeroed(height: usize, width: usize) -> Self {
+        Self {
+            dims: (height, width),
+            data: vec![T::default(); height * width],
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> &T {
+        &self.data[x*self.dims.1 + y]
+    }
+}
+
 fn colormap(value: f64, colormap: &[[u8; 4]]) -> [u8; 4] {
     let idx = (255. * value) as usize;
     colormap[idx]
@@ -20,14 +39,9 @@ fn sink_edges(value: f64, xnorm: f64, ynorm: f64) -> f64 {
     if output < 0.0 { 0.0 } else { output }
 }
 
-// Return cardinal neighbors of point, [West, North, East, South]
+// Return cardinal neighbors of point, [North, East, South, West]
 fn get_neighbors(point: (usize, usize), xmax: usize, ymax: usize) -> Vec<Option<(usize, usize)>> {
     let mut output = vec![];
-    if point.0 > 0 {
-        output.push(Some((point.0 - 1, point.1)));
-    } else {
-        output.push(None);
-    }
     if point.1 > 0 {
         output.push(Some((point.0, point.1 - 1)));
     } else {
@@ -40,6 +54,11 @@ fn get_neighbors(point: (usize, usize), xmax: usize, ymax: usize) -> Vec<Option<
     }
     if point.1 < ymax - 1 {
         output.push(Some((point.0, point.1 + 1)));
+    } else {
+        output.push(None);
+    }
+    if point.0 > 0 {
+        output.push(Some((point.0 - 1, point.1)));
     } else {
         output.push(None);
     }
@@ -219,9 +238,104 @@ fn heightmap_to_texture(map: &[Vec<f64>]) -> Texture2D {
     Texture2D::from_rgba8(w as u16, h as u16, &rgba)
 }
 
+fn normal(pos: (usize, usize), map: &Map<f64>) -> (f64, f64) {
+    let h = map.get(pos.0, pos.1);
+    let dims = map.dims;
+    let n = get_neighbors(pos, dims.0, dims.1);
+    let mut dy = 0.0;
+    if let Some(nn) = n[0] {
+        // North
+        let other_h = map.get(nn.0, nn.1);
+        dy += (other_h - h) / 2.0;
+    }
+    if let Some(nn) = n[2] {
+        // South
+        let other_h = map.get(nn.0, nn.1);
+        dy += (h - other_h) / 2.0;
+    }
+    let mut dx = 0.0;
+    if let Some(nn) = n[1] {
+        // East
+        let other_h = map.get(nn.0, nn.1);
+        dx += (other_h - h) / 2.0;
+    }
+    if let Some(nn) = n[3] {
+        // West
+        let other_h = map.get(nn.0, nn.1);
+        dx += (h - other_h) / 2.0;
+    }
+    (dx, dy)
+}
+
+#[derive(Clone, Copy, Debug)]
+struct WaterUnit {
+    volume: f64,
+    sediment: f64,
+    density: f64,
+    pos: (usize, usize),
+    velocity: (f64, f64),
+}
+
+impl WaterUnit {
+    fn new(x: usize, y:usize) -> Self {
+        Self {
+            volume: 1.0,
+            sediment: 0.0,
+            density: 1.0,
+            pos: (x, y),
+            velocity: (0.0, 0.0),
+        }
+    }
+
+    fn descend(&mut self, map: &mut Map<f64>, track: &mut Map<bool>) {
+        let dt = 0.1;
+        let friction = 0.1;
+        let h0 = map.get(self.pos.0, self.pos.1);
+        let n = normal(self.pos, map);
+        let mass = self.volume * self.density;
+        self.velocity.0 += dt * n.0 / mass;
+        self.velocity.1 += dt * n.1 / mass;
+        self.pos.0 = (self.pos.0 as f64  + dt * self.velocity.0) as usize;
+        self.pos.1 = (self.pos.1 as f64  + dt * self.velocity.1) as usize;
+        self.velocity.0 *= 1.0 - dt * friction;
+        self.velocity.1 *= 1.0 - dt * friction;
+        let h1 = map.get(self.pos.0, self.pos.1);
+        let speed: f64 = (self.velocity.0.powi(2) + self.velocity.1.powi(2)).sqrt();
+        let mut ceq = self.volume * speed * (h0 - h1);
+        if ceq < 0.0 {
+            ceq = 0.0;
+        }
+        let cdiff = ceq - 
+    }
+
+    fn flood(&mut self, map: &mut [Vec<f64>]) {
+
+    }
+}
+
+fn hydraulic_erosion_iteration2(map: &mut [Vec<f64>], stream: &mut [Vec<f64>], pool: &mut [Vec<f64>], track: &mut [Vec<bool>], steps: usize) {
+    for i in 0..steps {
+        let mut x = ::rand::random_range(1..=map[0].len() - 1);
+        let mut y = ::rand::random_range(1..=map.len() - 1);
+        let mut drop = WaterUnit::new(x, y);
+    }
+}
+
 #[macroquad::main("RPCG")]
 async fn main() {
+    let mut w = screen_width();
+    let mut h = screen_height();
+    request_new_screen_size(1920., 1080.);
     let (mut heightmap, mut before) = generate_heightmap();
+
+    // Track tells where water ran this iteration
+    let mut track = Map::<bool>::zeroed(w as usize, h as usize);
+
+    // Pool tells where pools are in the grid, and how deep they are.
+    let mut pool = Map::<f64>::zeroed(w as usize, h as usize);
+
+    // Stream tells where water is moving in the grid, and how deep they are.
+    let mut stream = Map::<f64>::zeroed(w as usize, h as usize);
 
     // Textures
     let mut before_texture = heightmap_to_texture(&before);
@@ -230,6 +344,13 @@ async fn main() {
     let mut screen = 0;
 
     loop {
+        if screen_width() != w || screen_height() != h {
+            w = screen_width();
+            h = screen_height();
+            (heightmap, before) = generate_heightmap();
+            before_texture = heightmap_to_texture(&before);
+            texture = heightmap_to_texture(&heightmap);
+        }
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
